e7eaef0161aa10687f593218f9daf7ed8c5f99f8
MartaGBayona <marta.g.bayona@outlook.es>
2024-03-02 11:12:50 +0100

    feat: start conexion on db successfully

---
+++ src/database/db.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import "dotenv/config"
+
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3312,
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_ROOT_PASSWORD || "",
+    database: process.env.DB_DATABASE ||"test",
+    entities: [],
+    migrations: [],
+    synchronize: false,
+    logging: false,
+})
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,23 @@
-import express, {Application} from "express";
-import dtenv from "dotenv";
+
+import "dotenv/config"
+
+import { AppDataSource } from "./database/db";
+import { app } from "./app";
+
+const PORT = process.env.PORT || 4001;
+
+const startServer = () => {
+    AppDataSource.initialize()
+        .then(() => {
+            console.log('DataBase connected');
+
+            app.listen(PORT, () => { 
+                console.log(`server is running on port: ${PORT}`);
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+startServer();
